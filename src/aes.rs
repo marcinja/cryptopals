0e628e41bff56319bdd97d99a6c3f5d7af646585
Marcin Jachymiak <marcin.jachymiak1@gmail.com>
2018-12-29 14:31:53 -0500

    Finish set 1

--- src/aes.rs
+++ src/aes.rs
@@ -1,4 +1,5 @@
 use base64;
+use hex;
 use openssl::symm::{decrypt, encrypt, Cipher};
 use std::fmt;
 use std::fs::File;
@@ -36,7 +37,7 @@ fn enc_dec_test() {
     }
 }
 
-#[test]
+#[test] // Challenge 7
 fn aes_encrypt() {
     let file_name: String =
         String::from("/home/marcinja/rustacean/cryptopals/data/challenge-7.txt");
@@ -54,4 +55,55 @@ fn aes_encrypt() {
     let dec = String::from_utf8(plaintext).unwrap();
 
     println!("{}", dec);
+}
+
+#[test]
+fn challenge_8() {
+    let file_name: String =
+        String::from("/home/marcinja/rustacean/cryptopals/data/challenge-8.txt");
+    let mut f = File::open(file_name).expect("file not found");
+
+    let mut input = String::new();
+    f.read_to_string(&mut input)
+        .expect("something went wrong reading the file");
+
+    let mut likely_line: &str = "";
+    let mut max_collisions: u32 = 0;
+    for l in input.lines() {
+        let data: Vec<u8> = hex::decode(l).unwrap();
+        let mut collisions = 0;
+
+        // Check how many 16 bytes blocks are exactly the same to detect ECB-mode.
+        let num_blocks = data.len() / 16;
+        for i in 0..num_blocks {
+            for j in (i + 1)..num_blocks {
+                let block1 = &data[i * 16..(i + 1) * 16];
+                let block2 = &data[j * 16..(j + 1) * 16];
+
+                if equal_blocks(block1, block2) {
+                    collisions += 1;
+                }
+            }
+        }
+
+        if collisions >= max_collisions {
+            likely_line = l;
+            max_collisions = collisions;
+        }
+    }
+
+    println!(
+        "Num collisions : {}\n, Line: \n {}",
+        max_collisions, likely_line
+    );
+}
+
+fn equal_blocks(x: &[u8], y: &[u8]) -> bool {
+    for i in 0..16 {
+        if x[i] != y[i] {
+            return false;
+        }
+    }
+
+    true
 }
